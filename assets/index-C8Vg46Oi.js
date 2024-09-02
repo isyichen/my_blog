8a0c3da73f9e73ec8ad795eeec32a016278a3db4
dagang <isgang@163.com>
2024-09-02 14:58:57 +0800

    搭建

---
+++ assets/index-C8Vg46Oi.js
@@ -0,0 +1,1 @@
+import{d as e}from"./index-OhFtdMCe.js";const r=e({name:"aboutPlatform",__name:"index",setup(t){return(n,a)=>" 关于 "}});export{r as default};
---
+++ assets/index-DAcRU3W7.js
@@ -0,0 +1,1 @@
+import{a7 as e,c as s,e as t}from"./index-OhFtdMCe.js";const n={class:"userCenter-container"},a=Object.assign({name:"userCenter"},{__name:"index",setup(c){return(r,o)=>(s(),t("div",n,"用户中心"))}}),d=e(a,[["__scopeId","data-v-b2205fd2"]]);export{d as default};
